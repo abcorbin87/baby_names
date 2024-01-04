f5bd0206a726a37d0e3ca5479393b4cbaf3db1ba
abcorbin87 <155682556+abcorbin87@users.noreply.github.com>
2024-01-04 14:59:12 -0600

    Add files via upload

--- app.R
+++ app.R
@@ -1,25 +1,10 @@
-# baby names app to generate random boy or girl name for family planning purposes. Data from babynames R application
-# andrew.corbin.ngs@gmail.com
+# andrew.corbin.ngs@gmail.com 
 
 library(shiny)
+library(tidyverse)
 library(babynames)
 library(dplyr)
-
-
-# 
-# M_names <- filter(babynames, sex == "M")
-# M_names <- as.data.frame(unique(M_names$name))
-# F_names <- filter(babynames, sex == "F")
-# F_names <- as.data.frame(unique(F_names$name))
-# 
-# 
-# F_names <- filter(babynames, sex == "F")
-# F_names <- filter(F_names, n > 1000)
-# F_names <- as.data.frame(unique(F_names$name))
-# 
-# M_names <- filter(babynames, sex == "M")
-# M_names <- filter(M_names, n > 1000)
-# M_names <- as.data.frame(unique(M_names$name))
+library(plotly)
 
 M_names <- babynames %>%
   filter(sex == "M") %>%
@@ -33,47 +18,51 @@ F_names <- babynames %>%
 
 F_names <- as.data.frame(unique(F_names$name))
 
-ui <- fluidPage(titlePanel("Baby Name Generator"),
-                actionButton("Fbutton", "Girl Name", class = "btn-info", style="color: #fff; background-color: #e44ef5; border-color: #2e6da4"),
-                actionButton("Mbutton", "Boy Name", class = "btn-info", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-                verbatimTextOutput("text")
-)
-server <- function(input, output){
+ui <- fluidPage(titlePanel("Baby Name Popularity Throughout Time"),
+                textInput(inputId = "name",
+                          label = "Name (Case sensitive):",
+                          value = "Kolt"),
+                actionButton("Fbutton", "Generate Random Girl Name", class = "btn-info", style="color: #fff; background-color: #e44ef5; border-color: #2e6da4"),
+                actionButton("Mbutton", "Generate Random Boy Name", class = "btn-info", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                verbatimTextOutput("text"),
+                selectInput(inputId = "sex",
+                            label = "Gender:",
+                            choices = list(Male = "M",
+                                           Female = "F"),
+                            selected = "M"),
+
+                sliderInput(inputId = "year",
+                            label =  "Year Range Slider:",
+                            min = min(babynames$year),
+                            max = max(babynames$year),
+                            value = c(min(babynames$year),
+                                        max(babynames$year)),
+                                    sep = ""),
+                plotlyOutput(outputId = "nameplot")
+                )
+
+server <- function(input, output) {
+  output$nameplot <- renderPlotly({
+    babynames %>%
+      filter(sex == input$sex,
+             name == input$name) %>%
+      ggplot(aes(x = year,
+                 y = n)) +
+      labs(y = "Number of People", 
+           x = "Year") +
+      geom_line() +
+      scale_x_continuous(limits = input$year) +
+      theme_minimal()  +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+  })
   v <- reactiveValues(data = NULL)
-  
   observeEvent(input$Fbutton, {
-    output$text <- renderText(sample(F_names$unique, 1))
+    output$text <- renderText(sample(F_names$`unique(F_names$name)`, 1))
   })
   observeEvent(input$Mbutton, {
-    output$text <- renderText(sample(M_names$unique, 1))
+    output$text <- renderText(sample(M_names$`unique(M_names$name)`, 1))
   })
 }
+shinyApp(ui = ui, server = server)
 
-shinyApp(ui=ui, server=server)
-
-########
-# pull male and female names
-# 
-# sex <- "M"
-# ocr <- 1000
-# M_names <- babynames %>%
-#   filter(sex == "M") %>%
-#   filter(n > ocr)
-# 
-# M_names <- as.data.frame(unique(M_names$name))
-# 
-# F_names <- babynames %>%
-#   filter(sex == "F")
-# 
-# 
-# 
-# F_names <- filter(babynames, sex == "F")
-# F_names <- filter(F_names, n > 1000)
-# F_names <- as.data.frame(unique(F_names$name))
-# 
-# M_names <- filter(babynames, sex == "M")
-# M_names <- filter(M_names, n > 1000)
-# M_names <- as.data.frame(unique(M_names$name))
-# 
-# head(M_names)
-# head(F_names)
